ea4d9f6f3e57191f478781efb5b162cbeafab809
rk000143 <149115433+rk000143@users.noreply.github.com>
2023-10-26 17:46:31 -0400

    Initial repository for the web app, included a basic hello world mapping

---
+++ src/test/java/edu/syr/group2/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.syr.group2.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
